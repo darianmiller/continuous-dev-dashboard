53f715144911a2737ca2014415106fc5be4aee2c
Darian Miller <darian@darianmiller.com>
2026-03-08 19:13:52 -0500

    Deployed 0b26741 with MkDocs version: 1.6.1

--- javascripts/tablesort.js
+++ javascripts/tablesort.js
@@ -1,4 +1,9 @@
 document$.subscribe(function () {
   var tables = document.querySelectorAll("article table")
-  tables.forEach(function (table) { new Tablesort(table) })
+  tables.forEach(function (table) {
+    var firstTh = table.querySelector("th")
+    if (firstTh && firstTh.textContent.trim() !== "") {
+      new Tablesort(table)
+    }
+  })
 })
